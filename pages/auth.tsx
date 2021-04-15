46e933c2f2c599fadb55a477a3a1c526926711c3
Emmett Harper <eharpe23@yahoo.com>
2021-04-15 12:53:10 -0700

    get react hook forms in and working

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,32 +6,77 @@ import {
   withAuthUserTokenSSR,
 } from "next-firebase-auth";
 import { useAuth } from "firebase/useAuth";
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const SignIn = () => {
   const AuthUser = useAuthUser();
   console.log("AuthUser", AuthUser);
-  const { signIn } = useAuth();
+  // const { signIn } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <>
       <div>
         <h1>Sign In</h1>
-        <div className="flex">
-          <label className="block">
-            <span className="text-gray-700">Email</span>
-            <input
-              className="form-input mt-1 block w-full"
-              placeholder="Jane Doe"
-            />
-          </label>
 
-          <label className="block">
-            <span className="text-gray-700">Password</span>
+        <form className="mb-6" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col mb-4">
+            <label
+              className="mb-2 uppercase font-bold text-lg text-grey-darkest"
+              htmlFor="email"
+            >
+              Email
+            </label>
             <input
-              className="form-input mt-1 block w-full"
-              placeholder="Jane Doe"
+              className="border py-2 px-3 text-grey-darkest"
+              type="email"
+              name="email"
+              id="email"
+              defaultValue="nick17@gmail.com"
+              {...register("email", { required: true })}
             />
-          </label>
-        </div>
+            {errors.email && <span>Valid Email is required</span>}
+          </div>
+          <div className="flex flex-col mb-6">
+            <label
+              className="mb-2 uppercase font-bold text-lg text-grey-darkest"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="border py-2 px-3 text-grey-darkest"
+              type="password"
+              name="password"
+              id="password"
+              {...register("password", { required: true })}
+            />
+            {errors.password && <span>Valid Password is required</span>}
+          </div>
+          <button
+            className="block bg-teal hover:bg-teal-dark text-white uppercase text-lg mx-auto p-4 rounded"
+            type="submit"
+          >
+            Log In
+          </button>
+        </form>
+        <a
+          className="block w-full text-center no-underline text-sm text-grey-dark hover:text-grey-darker"
+          href="/login"
+        >
+          Already have an account?
+        </a>
       </div>
     </>
   );
