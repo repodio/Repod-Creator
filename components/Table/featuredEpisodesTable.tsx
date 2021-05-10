78c041aad8b163abd6d8b7a9ff642e80cefdafc5
Emmett Harper <eharpe23@yahoo.com>
2021-05-10 11:38:31 -0700

    get the mobile featured episodes page working

--- components/Table/featuredEpisodesTable.tsx
+++ components/Table/featuredEpisodesTable.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import Table from "./table";
 import EmptyTable from "./emptyTable";
+import { useMediaQuery } from "react-responsive";
 
 import { fromNow } from "utils/formats";
 import { Button } from "components/Buttons";
 
 const FeaturedEpisodesTable = ({ data, onClick, loading }) => {
-  const columns = React.useMemo(
-    () => [
+  const isMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+
+  const columns = React.useMemo(() => {
+    if (isMobile) {
+      return [
+        {
+          Header: "Title",
+          accessor: "title",
+        },
+        {
+          Header: "",
+          accessor: "episodeId",
+          width: 48,
+          Cell: (row) => (
+            <div className="text-right px-4">
+              <Button.Tiny
+                className={`bg-repod-tint text-repod-text-alternative border-2 border-repod-tint`}
+                onClick={() => {
+                  onClick(row.value);
+                }}
+              >
+                Select
+              </Button.Tiny>
+            </div>
+          ),
+        },
+      ];
+    }
+
+    return [
       {
         Header: "",
         Cell: (row) => (
@@ -50,9 +79,8 @@ const FeaturedEpisodesTable = ({ data, onClick, loading }) => {
           </div>
         ),
       },
-    ],
-    []
-  );
+    ];
+  }, [isMobile]);
 
   return data && data.length ? (
     <Table data={data} columns={columns} loading={loading} />
