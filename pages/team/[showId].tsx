242d40fa54832c54039d320f42d9b3e1e2a8273a
Emmett Harper <eharpe23@yahoo.com>
2021-05-31 13:15:45 -0700

    basica team members table ui

--- pages/team/[showId].tsx
+++ pages/team/[showId].tsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { withAuthUser, AuthAction } from "next-firebase-auth";
 import { useSelector, useDispatch } from "react-redux";
 import { selectors as showsSelectors } from "modules/Shows";
 import { selectors as authSelectors } from "modules/Auth";
 import { useRouter } from "next/router";
-import { LoadingScreen } from "components/Loading";
+import { Loader, LoadingScreen } from "components/Loading";
 import { ThunkDispatch } from "redux-thunk";
 import { Action } from "redux";
 import { TeamMembersTable } from "components/Table";
 import { TeamLayout } from "components/Layouts";
 import { useMediaQuery } from "react-responsive";
+import {
+  selectors as profileSelectors,
+  fetchTeamMembers,
+} from "modules/Profile";
 
 const Team = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
   const { showId } = router.query;
   const showIdString = showId as string;
 
@@ -20,22 +25,27 @@ const Team = () => {
   const authProfile = useSelector(authSelectors.getAuthedProfile);
   const dispatch = useDispatch<ThunkDispatch<{}, undefined, Action>>();
   const isMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const teamMembers = useSelector(
+    profileSelectors.getTeamMemberProfiles(showId)
+  );
 
-  // useEffect(() => {
-  //   (async () => {
-  //     if (!show) {
-  //       router.replace(`/`);
-  //     }
-  //     await dispatch(fetchShowStats(showId));
-  //   })();
-  // }, []);
+  useEffect(() => {
+    (async () => {
+      if (!show) {
+        router.replace(`/`);
+      }
+      await dispatch(fetchTeamMembers(showIdString));
+      setLoading(false);
+    })();
+  }, []);
 
   if (!show) {
     return <LoadingScreen />;
   }
 
   const authProfileClaimedShow = show.claimedShow.users[authProfile.userId];
-  console.log("authProfileClaimedShow", authProfileClaimedShow);
+  console.log("show", show);
+  console.log("teamMembers", teamMembers);
 
   const authProfileWithRoles = {
     ...authProfile,
@@ -47,26 +57,18 @@ const Team = () => {
     <TeamLayout>
       <div className="flex flex-col">
         <p className="text-2xl font-bold text-repod-text-primary">You</p>
-        {/* <div className="flex flex-row my-8 border-red-500 border">
-          <div className="flex-0 mr-2">
-            <ProfileAvatar url={authProfile.avatarUrl} />
-          </div>
-          <div className="flex-1">
-            <p className="text-md font-bold text-repod-text-primary">
-              {authProfile.displayName}
-            </p>
-            <p className="text-sm text-repod-text-secondary">
-              {authProfile.email || "-"}
-            </p>
-          </div>
-          <p>{authProfileClaimedShow}</p>
-          <p></p>
-        </div> */}
+        <TeamMembersTable data={[authProfileWithRoles]} />
 
-        <TeamMembersTable
-          data={[authProfileWithRoles]}
-          // loading={loadingEpisodes}
-        />
+        {loading ? (
+          <Loader />
+        ) : teamMembers.length ? (
+          <>
+            <p className="text-2xl font-bold text-repod-text-primary mt-8">
+              Members
+            </p>
+            <TeamMembersTable data={teamMembers} />
+          </>
+        ) : null}
       </div>
     </TeamLayout>
   );
