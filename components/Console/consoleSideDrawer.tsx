36f9165f8dd471e04f53d8b7836cd96859722d9e
Emmett Harper <eharpe23@yahoo.com>
2021-12-23 13:24:53 -0800

    fix side icon

--- components/Console/consoleSideDrawer.tsx
+++ components/Console/consoleSideDrawer.tsx
@@ -22,6 +22,7 @@ const NavigationLink = ({
   label = "",
   isSelected = false,
   expanded,
+  useStrokeOverFill = false,
 }) => (
   <Link href={destination}>
     <a
@@ -32,9 +33,10 @@ const NavigationLink = ({
       {expanded ? (
         <div className="w-full p-3 flex flex-row">
           <IconComponent
-            className={`mr-2 fill-current transition ${
+            className={`mr-2 transition ${
               isSelected ? "text-repod-tint" : "text-repod-text-alternative"
-            }`}
+            }
+             ${useStrokeOverFill ? " stroke-current" : " fill-current"}`}
             size={24}
           />
 
@@ -49,9 +51,10 @@ const NavigationLink = ({
       ) : (
         <div className="w-full m-3 flex flex-row">
           <IconComponent
-            className={`mr-2 fill-current transition ${
+            className={`mr-2 transition ${
               isSelected ? "text-repod-tint" : "text-repod-text-alternative"
-            }`}
+            }
+            ${useStrokeOverFill ? " stroke-current" : " fill-current"}`}
             size={24}
           />
         </div>
@@ -111,6 +114,7 @@ const ConsoleSideDrawer = () => {
           destination={`/${router.query.showId}/episodes/`}
           label="Episodes"
           IconComponent={Mic}
+          useStrokeOverFill
           expanded={expanded}
         />
         <NavigationLink
