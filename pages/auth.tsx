e7276e1661b59d9666481f3b673136ee1c9e188a
Emmett Harper <eharpe23@yahoo.com>
2022-02-03 09:41:41 -0800

    add in too-many-requests error for us to identify

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -36,6 +36,7 @@ const AUTH_ERROR_CODES = {
   "auth/user-disabled": Copy.Auth.userDisabled,
   "auth/invalid-verification-code": Copy.Auth.invalidVerificationCode,
   "auth/invalid-verification-id": Copy.Auth.invalidVerificationId,
+  "auth/too-many-requests": Copy.Auth.tooManyRequests,
 };
 
 const SignUp = ({ handleToggleSignupMode, authError, setAuthError }) => {
@@ -135,7 +136,10 @@ const LogIn = ({ handleToggleSignupMode, authError, setAuthError }) => {
       const errorMessage =
         AUTH_ERROR_CODES[response.error.code] || Copy.Auth.defaultError;
       setAuthError(errorMessage);
-      logEvent(analyticsEvents.login_error, response.error);
+      logEvent(analyticsEvents.login_error, {
+        ...(response.error || {}),
+        email,
+      });
     }
   };
 
